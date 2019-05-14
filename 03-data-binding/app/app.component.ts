f4e89ee77ea257b1d459194e155c3fced454f138
Thiago Castilho <tcastilho@outlook.com>
2019-05-14 15:34:33 -0300

    files from classes 8 to 10

---
+++ 03-data-binding/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { DataBindingComponent } from './data-binding/data-binding.component'
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <exemplo-data-binding></exemplo-data-binding>
+    `,
+    directives: [DataBindingComponent]
+})
+export class AppComponent { }
---
+++ 03-data-binding/app/data-binding/data-binding.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'exemplo-data-binding',
+    templateUrl: 'data-binding.component.html'
+})
+
+export class DataBindingComponent {
+    constructor() { }
+
+    url = 'http://loiane.com'
+    urlImg = 'http://lorempixel.com/400/200/nature/'
+
+    getValor() {
+        return 1;
+    }
+}
